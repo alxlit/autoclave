257984c5496b4572646610e691916a9ab278894c
Alex Little <alxlit@gmail.com>
2013-06-09 17:19:21 -0400

    add line anchor prefix for codox

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :codox {:output-dir "doc/codox"
-          :src-dir-uri "http://github.com/alxlit/autoclave/blob/master"}
+          :src-dir-uri "http://github.com/alxlit/autoclave/blob/master"
+          :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "r173"]]
 

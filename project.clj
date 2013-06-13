1b8bfee184a3a33beb8787a687e1752ced1bcd1b
Alex Little <alxlit@gmail.com>
2013-06-12 23:01:14 -0400

    bump version again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autoclave "0.1.1"
+(defproject autoclave "0.1.2"
   :description "A library for sanitizing various kinds of user input."
   :url "http://github.com/alxlit/autoclave"
   :license {:name "Eclipse Public License"
@@ -8,8 +8,5 @@
           :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "r173"]]
-
-  ; JSON Sanitizer is included locally as it isn't available from any
-  ; repositories, as far as I know.
-  :resource-paths ["resources/json-sanitizer-2012-10-17.jar"])
+  :java-source-paths ["src/java"])
 

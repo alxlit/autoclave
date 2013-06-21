c7704ae3a5bdcc6a8b98c1150f7661febb356449
Alex Little <alxlit@gmail.com>
2013-06-21 14:53:51 -0400

    bump version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject autoclave "0.1.2"
-  :description "A library for sanitizing various kinds of user input."
+(defproject autoclave "0.1.4"
+  :description "A library for safely handling various kinds of user input."
   :url "http://github.com/alxlit/autoclave"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,6 +7,7 @@
           :src-dir-uri "http://github.com/alxlit/autoclave/blob/master"
           :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.pegdown/pegdown "1.3.0"]
                  [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "r173"]]
   :java-source-paths ["src/java"])
 

bc67b15edb2b5e884db520925aadb1087f298d68
Alex Little <alxlit+github@alxlit.name>
2014-04-30 13:02:32 -0400

    fix source uri

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :codox {:output-dir "doc/codox"
-          :src-dir-uri "http://github.com/alxlit/autoclave/blob/master"
+          :src-dir-uri "http://github.com/alxlit/autoclave/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.pegdown/pegdown "1.4.2"]

b24fb4de739ecebdedf907787a203616dbc29946
Alexander Little <alxlit+github@alxlit.name>
2016-03-26 13:19:54 -0400

    bump library versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autoclave "0.1.7"
+(defproject alxlit/autoclave "0.2.0"
   :description "A library for safely handling various kinds of user input."
   :url "http://github.com/alxlit/autoclave"
   :license {:name "Eclipse Public License"
@@ -7,8 +7,15 @@
           :src-dir-uri "http://github.com/alxlit/autoclave/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.pegdown/pegdown "1.4.2"]
-                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "r239" :exclusions [com.google.guava/guava com.google.code.findbugs/jsr305]]]
-  :java-source-paths ["src/java"])
+                 [org.pegdown/pegdown "1.6.0"]
+                 [commons-codec "1.4"]
+                 [com.google.guava/guava "11.0"]
+                 [com.mikesamuel/json-sanitizer "1.1"
+                  :exclusions [com.google.guava/guava
+                               com.google.code.findbugs/jsr305]]
+                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20160203.1"
+                  :exclusions [commons-codec
+                               com.google.guava/guava
+                               com.google.code.findbugs/jsr305]]])
 
 
